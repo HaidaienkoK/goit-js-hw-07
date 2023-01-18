c8abb2b2bb013c3799303bbdc87f60ed2ea68de3
HaidaienkoK <gaidk1973@gmail.com>
2023-01-18 21:58:27 +0200

    end example two

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const imagesContainer = document.querySelector(".gallery");
+
+const cardsImage = createImageCards(galleryItems);
+imagesContainer.insertAdjacentHTML("beforeend", cardsImage);
+
+// imagesContainer.addEventListener("click", onClickContainerImage);
+
+function createImageCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `  
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+    .join("");
+}
+
+    (function () {
+        var $gallery = new SimpleLightbox(".gallery a", {
+          captionType: "attr",
+          captionsData: "alt",
+          captionPosition: "bottom",
+          captionDelay:250,
+        });
+    })();
+

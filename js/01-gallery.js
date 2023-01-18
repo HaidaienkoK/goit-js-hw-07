55e84233e11bc4bd43f7ed8aaa0029c6e42c5f8a
HaidaienkoK <gaidk1973@gmail.com>
2023-01-18 18:09:40 +0200

    end part one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const imagesContainer = document.querySelector(".gallery");
 
+const cardsImage = createImageCards(galleryItems);
+imagesContainer.insertAdjacentHTML("beforeend", cardsImage);
+
+imagesContainer.addEventListener("click", onClickContainerImage);
+
+function createImageCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `     
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+}
+
+function onClickContainerImage(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`);
+  instance.show();
+
+  imagesContainer.addEventListener(
+    "keydown",
+    (event) => {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+}
